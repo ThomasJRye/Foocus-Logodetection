edaafa7a108902b18226b13abd183d53bec917e6
Thomas Rye <thomasrye@Thomass-MacBook-Pro.local>
2023-04-27 11:13:34 +0200

    does not run

---
+++ main.py
@@ -0,0 +1,55 @@
+import torch
+import config
+from utils import (
+    collate_fn,
+    get_transform,
+    myOwnDataset,
+)
+import model
+import json
+import torchvision.transforms as transforms
+from torch.utils.tensorboard import SummaryWriter
+from training import train_model
+from eval import evaluate_model
+import datetime, os
+# Load json file
+with open(config.test_coco) as f:
+    json_data = json.load(f)
+
+# Crate test Dataset
+testing_dataset = myOwnDataset(
+    root=config.data_dir, annotation=config.test_coco, transforms=get_transform()
+)
+
+# Testing DataLoader
+testing_loader = torch.utils.data.DataLoader(
+    testing_dataset,
+    batch_size=config.test_batch_size,
+    shuffle=config.train_shuffle_dl,
+    num_workers=config.num_workers_dl,
+    collate_fn=collate_fn,
+)
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+logdir = os.path.join("logs", datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+
+# Instantiate and train the model
+notransform_model = model.get_model(device=device, model_name='v2')
+
+writer = SummaryWriter()
+
+# Train the model
+train_model(notransform_model, device, writer)
+
+# Close the SummaryWriter
+writer.close()
+
+# Evaluate the model
+evaluate_model(notransform_model, device, testing_loader, "notrans")
+
+# # Load the TensorBoard notebook extension
+# %load_ext tensorboard
+
+# # Start TensorBoard
+# %tensorboard --logdir logs
--- model.py
+++ model.py
@@ -1,11 +1,9 @@
 import torchvision
-import tensorflow as tf
 
-def get_model(device='cpu', model_name='v2', tensorboard_callback=None):
+def get_model(device='cuda', model_name='v2', tensorboard_callback=None):
     # Load the model.
     if model_name == 'v2':
         model = torchvision.models.detection.fasterrcnn_resnet50_fpn_v2(
-            pretrained=True,
             num_classes=33
         )
     elif model_name == 'v1':
@@ -17,7 +15,7 @@ def get_model(device='cpu', model_name='v2', tensorboard_callback=None):
     model = model.eval().to(device)
 
     if tensorboard_callback is not None:
-        # Add the TensorBoard callback to the model's list of callbacks
+        # Add the TensorBoard callback to the modesl's list of callbacks
         callbacks = [tensorboard_callback]
         if model.training:
             model.train_callback_list = callbacks
